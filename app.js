3bb7cdc4959d84048bcabe13dfea2f0596a4048e
Aaron Bieber <qbit@qbit.io>
2012-01-27 16:02:43 -0800

    db creation working

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var sqlite = require( 'node-sqlite3' );
-var xmpp = require( 'node-xmpp' );
+var sqlite = require( 'node-sqlite3' ),
+	xmpp = require( 'node-xmpp' ),
+	fs = require( 'fs' ),
+	db_file = __dirname + '/timmy.db',
+	db = new sqlite.Database( db_file );
 
-var db = new sqlite.Database( 'timmy.db' );
+
+fs.stat( db_file, function( err, stat ) {
+	if ( stat.size === 0 ) {
+		console.log( "Creating db..." );
+		db.serialize( function() {
+			db.run( 'create table categories ( id integer primary key, name text )' );
+			db.run( 'create table projects( id integer primary key, name text )' );
+			db.run( 'create table times( id integer primary key, name text, categoryid integer, projectid integer, begin timestamp, end timestamp )' );
+		});
+	}
+});
